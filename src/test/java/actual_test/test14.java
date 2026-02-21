df037cff755d3021ab7f978edbdb8676b3db1b9a
Mani <manikumar7864@gmail.com>
2026-02-21 12:51:01 +0530

    Run selenium in headless CI

--- src/test/java/actual_test/test14.java
+++ src/test/java/actual_test/test14.java
@@ -6,30 +6,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
 public class test14 {
 
 
 
-		
-		@Test
-		public void test() throws Throwable {
-			
-			WebDriverManager.firefoxdriver().setup();
-			
-			WebDriver driver=new FirefoxDriver();
-			
-			driver.manage().window().maximize();
-			driver.get("https://demo.guru99.com/V4/");
-			driver.findElement(By.name("uid")).sendKeys("mngr652986");
-			driver.findElement(By.name("password")).sendKeys("jujygEt");
-			driver.findElement(By.name("btnLogin")).click();
-			Thread.sleep(4000);
-			
-			
-			driver.findElement(By.className("ghkb"));
-			driver.findElement(By.className("ghkb"));
 
-			driver.findElement(By.className("ghkb"));
-// jlskjfdlkfl
-		driver.close();
-}}
+	@Test
+	public void test() throws Throwable {
+
+	    WebDriverManager.firefoxdriver().setup();
+
+	    FirefoxOptions options = new FirefoxOptions();
+	    options.addArguments("--headless");   // runs in CI server
+
+	    WebDriver driver = new FirefoxDriver(options);
+
+	    driver.manage().window().maximize();
+	    driver.get("https://demo.guru99.com/V4/");
+
+	    driver.findElement(By.name("uid")).sendKeys("mngr652986");
+	    driver.findElement(By.name("password")).sendKeys("jujygEt");
+	    driver.findElement(By.name("btnLogin")).click();
+
+	    Thread.sleep(4000);
+
+	    driver.quit();
+	}
+}
