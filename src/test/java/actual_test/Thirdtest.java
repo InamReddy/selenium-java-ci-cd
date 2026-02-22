af9e485eba644709d492dea6a20fca2069786237
Mani <manikumar7864@gmail.com>
2026-02-22 12:23:13 +0530

    Run chrome in headless mode

--- src/test/java/actual_test/Thirdtest.java
+++ src/test/java/actual_test/Thirdtest.java
@@ -3,24 +3,30 @@ package actual_test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Thirdtest {
 
+    @Test
+    public void test2() {
 
-	@Test 
-	public void test2() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.flipkart.com/");
-		
-		driver.findElement(By.name("q")).sendKeys("Hp laptops");
-		
-		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
-		
-	}
+        WebDriverManager.chromedriver().setup();
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");     // VERY IMPORTANT
+        options.addArguments("--no-sandbox");       // Required in Linux CI
+        options.addArguments("--disable-dev-shm-usage"); // Prevent crash
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.flipkart.com/");
+
+        driver.findElement(By.name("q")).sendKeys("Hp laptops");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        driver.quit();
+    }
 }
