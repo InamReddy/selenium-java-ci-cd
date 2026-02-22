ae1f6ddfe763b6d6847ddf4a4c83f7657d222727
Mani <manikumar7864@gmail.com>
2026-02-22 12:02:47 +0530

    Testing 25

---
+++ src/test/java/actual_test/Thirdtest.java
@@ -0,0 +1,26 @@
+package actual_test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Thirdtest {
+
+
+	@Test 
+	public void test2() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		
+		driver.findElement(By.name("q")).sendKeys("Hp laptops");
+		
+		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		
+	}
+
+}
